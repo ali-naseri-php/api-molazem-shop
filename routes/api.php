319c46e0b01dd9cdd70eb76765c25cf7faa37462
ali <ali.naseri.php@gmail.com>
2025-01-01 12:41:24 +0330

    add route in api

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('product',[\App\Http\Controllers\Api\Product\ProductController::class,'index']);
-Route::get('product/{id}',[\App\Http\Controllers\Api\Product\ProductController::class,'show']);
+
+Route::prefix('product')->group(function () {
+     Route::get('', [\App\Http\Controllers\Api\Product\ProductController::class, 'index']);
+     Route::get('/{id}', [\App\Http\Controllers\Api\Product\ProductController::class, 'show']);
+     Route::post('', [\App\Http\Controllers\Api\Product\ProductController::class, 'update']);
+     Route::put('/{id}', [\App\Http\Controllers\Api\Product\ProductController::class, 'update']);
+     Route::delete('/{id}', [\App\Http\Controllers\Api\Product\ProductController::class, 'update']);
+});
